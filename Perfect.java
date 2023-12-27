382cb3f0370d5c46daa2aadcddfb61baacd2450c
Tamar Reuveny <Tamar.reuveny@runi.ac.il>
2023-12-27 15:44:24 +0200

    edit3

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,39 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		
+		int n = Integer.parseInt(args[0]);
+
+		int x = 0;
+
+		for ( int i = 1 ; i < n ; i ++) {
+
+			if (n % i == 0) {
+
+				x = x + i;
+			}
+		}
+
+			if (n == x) {
+
+				System.out.print(n + " is a perfect number since " + n + " = 1 ");
+
+				for ( int i = 2 ; i < n ; i ++) {
+
+			         if (n % i == 0) {
+
+			         	System.out.print(" + "+ i);
+
+			         	
+			         }
+			     }
+			 } else {
+
+			 	System.out.println(n + " is not a perfect number");
+			 }
+	   
+    }
 }
+
+	
+
--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,22 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+
+		String w = args[0];
+
+		int i = w.length() - 1;
+
+		while (i >= 0) {
+
+			System.out.print(w.charAt(i));
+
+			i--;
+
+		}
+
+		System.out.println();
+
+		System.out.println(w.charAt((w.length()-1)/2));
+
 	}
 }
